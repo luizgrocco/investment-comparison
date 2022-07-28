9a6ada46d3161b6402ee68b83a944b239b7a3c7b
Luiz Rocco <luizgustavorocco@hotmail.com>
2022-07-28 00:26:01 -0300

    Girlfriend commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { theme } from "./themes";
+import gigi from "./assets/gigi.png";
 
 const App: React.FC = () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -10,7 +11,22 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>Oi Giovanna, vc é minha 🦒inha!</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ textAlign: "center", marginBottom: "50px" }}>
+          Oi Giovanna, vc é minha 🦒inha!
+        </div>
+        <div
+          style={{ display: "flex", width: "100%", justifyContent: "center" }}
+        >
+          <img src={gigi} alt="Imagem da Girafinha" width={500} height={500} />
+        </div>
+      </div>
     </ThemeProvider>
   );
 };
