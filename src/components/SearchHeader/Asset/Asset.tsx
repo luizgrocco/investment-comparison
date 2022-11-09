775e3c6d4f37008aae782ce69a5131c61ce0ddb6
Luiz Rocco <luizgustavorocco@hotmail.com>
2022-11-09 02:12:19 -0300

    Beggining of asset UI implementation

--- src/components/SearchHeader/Asset/Asset.tsx
+++ src/components/SearchHeader/Asset/Asset.tsx
@@ -3,10 +3,19 @@ import { AssetContainer } from "./styles";
 
 import { AssetType } from "../../../api/rest-api";
 
+import { addAssetToDefaultPortfolio } from "./../../../redux/reducers";
+import { useAppDispatch } from "../../../redux/hooks";
+
 interface AssetComponentType {
   asset: AssetType;
 }
 
 export const Asset: React.FC<AssetComponentType> = ({ asset }) => {
-  return <AssetContainer>{asset.label}</AssetContainer>;
+  const dispatch = useAppDispatch();
+
+  const addAsset = (): void => {
+    dispatch(addAssetToDefaultPortfolio(asset));
+  };
+
+  return <AssetContainer onClick={addAsset}>{asset.label}</AssetContainer>;
 };
